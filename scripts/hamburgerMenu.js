cc2e8c8aacb6585113acd324fc9f6c968c72cd08
sudo430 <tyler@bee-eee.com>
2024-05-08 14:54:32 -0600

    did the team activity

--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -61,3 +61,12 @@ toggleModeButton.addEventListener("click", () => {
         lightMode = true;
     }
 })
+
+
+//counts the number of visits the user has made
+const visitCount = document.querySelector("#visit-count");
+
+let count = 1 + JSON.parse(localStorage.getItem("visitCount"));
+visitCount.innerHTML = count;
+localStorage.setItem("visitCount", JSON.stringify(count))
+
