1a311b20e292e0b62e5f9f8aceac939712b696b2
sudo430 <tyler@bee-eee.com>
2024-05-24 16:21:34 -0600

    finished directory page

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -18,23 +18,58 @@ async function getMembers(){
 getMembers();
 
 function displayMembers(data){
-    const div = document.querySelector("#comp-dir");
+    const div = document.querySelector("#comp-dir-gallary");
+    const table = document.querySelector("#list");
+    const mTable = document.querySelector("#m-list");
     div.innerHTML = "";
+    table.innerHTML = "";
+    mTable.innerHTML = "";
 
     data.members.forEach((memberData) => {
+        //populate cards
         const member = document.createElement("section");
-        member.className = "comp-card";
         const img = document.createElement("img");
-        const description = document.createElement("p");
+        const description = document.createElement("span");
 
-        img.setAttribute("src", "https://picsum.photos/100/100");
+        img.setAttribute("src", memberData.imageFileName);
         img.setAttribute("alt", `${memberData.name} logo`);
         
-        description.innerHTML = `<p>${memberData.name}<br>${memberData.addresse}<br>${memberData.phone}<br><a href="${memberData.url}">${memberData.url}</a></p>`;
+        description.innerHTML = `<h3>${memberData.name}</h3>${memberData.addresse}<br>${memberData.phone}<br><a href="${memberData.url}">${memberData.url}</a>`;
 
+        member.className = "comp-card";
         member.appendChild(img);
         member.appendChild(description);
 
         div.appendChild(member);
+
+        //populate table
+        
+        const tableRow = document.createElement("tr");
+        tableRow.innerHTML = `<td>${memberData.name}</td><td>${memberData.addresse}</td><td>${memberData.phone}</td><td><a href="${memberData.url}">${memberData.url}</a></td>`;
+
+        
+        table.appendChild(tableRow);
+
+        //populate mobile table
+        const mTableRow = document.createElement("tr");
+        mTableRow.innerHTML = `<td>${memberData.name}<br>${memberData.addresse}<br>${memberData.phone}<br><a href="${memberData.url}">${memberData.url}</a></td>`;
+        mTable.appendChild(mTableRow);
+        
+
     });
 }
+
+document.querySelector("#toggle-veiw").addEventListener("click", (button) => {
+    if(button.target.textContent === "List"){
+        button.target.textContent = "Gallary";
+        document.querySelector("#comp-dir-gallary").className = "hide";
+        document.querySelector("#comp-dir-list").className = "show";
+        document.querySelector("#m-comp-dir-list").className = "show";
+    }
+    else{
+        button.target.textContent = "List"
+        document.querySelector("#comp-dir-gallary").className = "show";
+        document.querySelector("#comp-dir-list").className = "hide";
+        document.querySelector("#m-comp-dir-list").className = "hide";
+    }
+});
